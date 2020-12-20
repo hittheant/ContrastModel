b41b58b5c3e23a189a6df29e20c4928d35944e3a
Adyant Balaji <adyantbalaji@Adyants-MacBook-Pro-2.local>
2020-12-19 21:12:50 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+
+import torchvision
+from tqdm import tqdm
+import torch
+import os
+from torch.utils.data import Dataset, DataLoader
+from pl_bolts.models.self_supervised import AMDIM
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Get image edge maps')
+    parser.add_argument('--data_dir', required=True, type=str, help='path to image data directory')
+    parser.add_argument('--save_dir', required=True, type=str, help='path to save resulting edge maps')
+    parser.add_argument('--saved_model_path', default='bsds500.pth', type=str, help='path of saved PyTorch model')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.save_dir):
+        os.mkdir(args.save_dir)
+
+    model = AMDIM(encoder='resnet18')
+
+    imagenet_data = torchvision.datasets.ImageNet('path/to/imagenet_root/')
+    data_loader = torch.utils.data.DataLoader(imagenet_data,
+                                              batch_size=64,
+                                              shuffle=True,
+                                              num_workers=2)
