49938cfa25031abb03bbbaa5bbb25493819bfa72
adyantbalaji <hittheant@gmail.com>
2021-01-11 01:37:47 -0500

    added transformations

--- dataset.py
+++ dataset.py
@@ -1,7 +1,8 @@
 import random
+
+from pl_bolts.models.self_supervised.simclr import SimCLRTrainDataTransform
 import utils
 from torch.utils.data import Dataset
-from torchvision import transforms
 from PIL import Image
 
 
@@ -14,19 +15,13 @@ class ImageFilesDataset(Dataset):
         random.shuffle(image_paths)
         self.image_paths = image_paths
 
-        transform = [transforms.Resize((64, 64))]
-        if training:
-            transform.append(transforms.RandomHorizontalFlip())
-        transform.append(transforms.ToTensor())
-        self.transform = transforms.Compose(transform)
-
-        self.nc = 1 if grayscale else 3
+        self.transform = SimCLRTrainDataTransform(input_height=32)
 
     def __getitem__(self, item):
         image = Image.open(self.image_paths[item])
         image = image.convert('L') if self.nc == 1 else image.convert('RGB')
-        image = self.transform(image)
-        return image
+        images = self.transform(image)
+        return images, None
 
     def __len__(self):
         return len(self.image_paths)
